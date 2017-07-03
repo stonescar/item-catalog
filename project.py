9d513ff77273ad7dc8d23d87f00de162efd2e5f6
Steinar <stonescar@gmail.com>
2017-07-03 23:00:19 +0200

    add category info to category specific API requests

--- project.py
+++ project.py
@@ -191,8 +191,9 @@ def allItemsJSON():
 @app.route('/category/<int:category_id>/JSON/')
 @helpers.category_exists
 def categoryJSON(category_id):
+    category = session.query(Category).filter_by(id=category_id).one()
     items = session.query(Item).filter_by(category_id=category_id).all()
-    return jsonify(Items=[i.serialize for i in items])
+    return jsonify(Category=category.serialize(items))
 
 
 @app.route('/login/')
